ed9e35930f6e8ac4e227450870ca2ac8c3875b79
dseflinger <dseflinger@gmail.com>
2025-06-26 16:59:55 -0700

    added primeng to app

--- src/app/features/job-applications/components/job-applications-list/job-applications-list.component.ts
+++ src/app/features/job-applications/components/job-applications-list/job-applications-list.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { TableModule } from 'primeng/table';
 
 @Component({
   selector: 'app-job-applications-list',
-  imports: [],
+  imports: [TableModule],
   templateUrl: './job-applications-list.component.html',
-  styleUrl: './job-applications-list.component.scss'
+  styleUrl: './job-applications-list.component.scss',
 })
 export class JobApplicationsListComponent {
 
